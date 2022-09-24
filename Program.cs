1d84b3c720c05a846dddf77a2ef82f9e323eb1be
eliza <eliza@DESKTOP-HLLIIPJ>
2022-09-24 15:05:27 +0300

    new

--- Program.cs
+++ Program.cs
@@ -8,6 +8,15 @@ namespace Лабораторна_1
 {
     internal class Program
     {
+        enum test1Status { Pass = 1, Fail = 2, Blocked = 3, WP = 4, Unexecuted = 5 }
+        enum HTTPError
+        {
+            Bad_Request = 400,
+            Unauthorized = 401,
+            Payment_Required = 402,
+            Forbidden = 403,
+            Not_Found = 404
+        }
         static void Main(string[] args)
         {
             //Task 1.1
@@ -16,8 +25,16 @@ namespace Лабораторна_1
             int month;
             day = Convert.ToInt32(Console.ReadLine());
             month = Convert.ToInt32(Console.ReadLine());
-            string result = (day > 0) && (day < 31) && (month > 0) && (month < 12) ? "Число може бути днем i мiсяцем" : "Число не може бути днем i мiсяцем";
+            string result = (day > 0) && (day <= 31) && (month > 0) && (month <= 12) ? "Число може бути днем i мiсяцем" : "Число не може бути днем i мiсяцем";
             Console.WriteLine(result);
+
+            //Task 1.2
+
+           /* int number;
+            number = Convert.ToInt32(Console.ReadLine());
+            string sum =
+            Console.WriteLine(sum);*/
+
 
             //Task 1.3
 
@@ -25,6 +42,34 @@ namespace Лабораторна_1
             hour = Convert.ToInt32(Console.ReadLine());
             string resul = (hour >= 5) && (hour < 11) ? "Доброго ранку!" : (hour >= 11) && (hour < 17) ? "Добрий день!" : (hour >= 17) && (hour <= 21) ? "Добрий вечір!" : "Доброї ночі!";
             Console.WriteLine(resul);
+
+            //Task 1.4
+
+            Console.WriteLine((test1Status)1);
+            Console.ReadKey();
+            Console.ReadLine();
+
+            //Task 2.1
+
+            float num1, num2, num3;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32((Console.ReadLine()));
+            num3 = Convert.ToInt32((Console.ReadLine()));
+            string check1 = (num1 >= -5) && (num1 <= 5) ? "num1 належить дiапазону" : "num1 не належить дiапазону";
+            string check2 = (num2 >= -5) && (num2 <= 5) ? "num2 належить дiапазону" : "num2 не належить дiапазону";
+            string check3 = (num3 >= -5) && (num3 <= 5) ? "num3 належить дiапазону" : "num3 не належить дiапазону";
+            Console.WriteLine(check1);
+            Console.WriteLine(check2);
+            Console.WriteLine(check3);
+
+            //Task 2.2
+            int numb1, numb2, numb3;
+            numb1 = Convert.ToInt32(Console.ReadLine());
+            numb2 = Convert.ToInt32(Console.ReadLine());
+            numb3 = Convert.ToInt32(Console.ReadLine());
+            int[] arr = { numb1, numb2, numb3 };
+            Console.WriteLine("min=" + arr.Min());
+            Console.WriteLine("max=" + arr.Max());
         }
     }
 
